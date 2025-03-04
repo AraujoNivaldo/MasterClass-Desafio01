f97cb926e8713f7eb24044cc6be975cff7c07755
Nivaldo Araujo <jr.souza.araujo@hotmail.com>
2025-03-04 16:29:54 -0300

    Adicionado novos casos de teste

--- cypress/e2e/Formulario-Cadastro.cy.js
+++ cypress/e2e/Formulario-Cadastro.cy.js
@@ -4,7 +4,49 @@ describe('Formulario de cadastro', () =>{
     cy.visit('/cadastro-simples.html')
   })
   it('Deve preencher o formulario com sucesso', ()=>{
-    cy.preencherFormulario()
+    cy.preencherFormulario({})
     cy.vericaConclusaoFormulario()
+  })
+  it('Campo nome deve ser obrigatorio', () =>{
+    cy.preencherFormulario({ nome:'{selectall}{backspace}'})
+    cy.validaPreenchimentoCampos('#name')
+  })
+  it('Campo email deve ser obrigatorio', () =>{
+    cy.preencherFormulario({ email:'{selectall}{backspace}'})
+    cy.validaPreenchimentoCampos('#email')
+  })
+  const emailsInvalidos = ['a', 'a@.', 'a@']
+  emailsInvalidos.forEach((email)=>{
+    it.only('Deve informar a digitacao incorreta do email', () =>{
+      cy.preencherFormulario({ email })
+      cy.verificarMensagemCampoEmail('#email', [ /Inclua um "@" no endereço de e-mail/,
+         /Insira uma parte depois de "@". "a@" está incompleto./,
+        /está sendo usado em uma posição incorreta em/])
+    })
+  })
+  
+  it('Campo senha deve ser obrigatorio', () =>{
+    cy.preencherFormulario({ password:'{selectall}{backspace}'})
+    cy.validaPreenchimentoCampos('#password')
+  })
+  it('Campo data de nascimento deve ser obrigatorio', () =>{
+    cy.preencherFormulario({ dataNsc:'{selectall}{backspace}'})
+    cy.validaPreenchimentoCampos('#birthdate')
+  })
+  it('Campo telefone deve ser obrigatorio', () =>{
+    cy.preencherFormulario({ phone:'{selectall}{backspace}'})
+    cy.validaPreenchimentoCampos('#phone')
+  })
+  it('Campo endereço deve ser obrigatorio', () =>{
+    cy.preencherFormulario({ address:'{selectall}{backspace}'})
+    cy.validaPreenchimentoCampos('#address')
+  })
+  it('Campo estado deve ser obrigatorio', () =>{
+    cy.preencherFormulario({ selecionarEstado: false})
+    cy.validaPreenchimentoCampos('#state')
+  })
+  it('Checkbox deve ser obrigatorio', () =>{
+    cy.preencherFormulario({ aceitarTermos: false})
+    cy.validaPreenchimentoCampos('#terms')
   })
 })
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,23 +1,26 @@
 import {faker} from "@faker-js/faker/locale/pt_BR"
 
-Cypress.Commands.add('preencherFormulario', (
-    nome = faker.person.fullName(), 
-    email = faker.internet.email(), 
-    password = faker.internet.password(8), 
-    dataNsc = faker.date.birthdate().toLocaleDateString('pt-BR'), 
-    phone = faker.helpers.replaceSymbols('##9########'), 
-    address = faker.location.streetAddress({useFullAddress: true})
+Cypress.Commands.add('preencherFormulario', ({
+    nome,
+    email,
+    password, 
+    dataNsc, 
+    phone, 
+    address,
+    selecionarEstado,
+    aceitarTermos
+    }
     ) =>{
-    cy.get('#name').type(nome || '{selectall}{backspace}')
-    cy.get('#email').type(email || '{selectall}{backspace}')
-    cy.get('#password').type(password || '{selectall}{backspace}', {log: false}) 
+    cy.get('#name').clear().type(nome ?? faker.person.fullName())
+    cy.get('#email').clear().type(email ?? faker.internet.email())
+    cy.get('#password').clear().type(password ?? faker.internet.password(8), {log: false}) 
     cy.selecionaItemLista('#gender')
-    cy.get('#birthdate').type(dataNsc || '{selectall}{backspace}')
-    cy.get('#phone').type(phone || '{selectall}{backspace}')
-    cy.get('#address').type(address || '{selectall}{backspace}')
-    cy.selecionaItemLista('#state')
+    cy.get('#birthdate').clear().type(dataNsc ?? faker.date.birthdate().toLocaleDateString('pt-BR'))
+    cy.get('#phone').clear().type(phone ?? faker.helpers.replaceSymbols('##9########'))
+    cy.get('#address').clear().type(address ?? faker.location.streetAddress({useFullAddress: true}))
+    selecionarEstado || cy.selecionaItemLista('#state')
     cy.selecionaItemLista('#city')
-    cy.get('#terms').click()
+    aceitarTermos || cy.get('#terms').click()
     cy.get('#cadastroForm > button').click()
 })
 
@@ -36,4 +39,20 @@ Cypress.Commands.add('selecionaItemLista', (seletor) => {
       cy.get(seletor).select(randomValue); 
     });
   });
-  
+
+Cypress.Commands.add('validaPreenchimentoCampos', (campo) =>{
+    cy.get(campo).invoke('prop', 'validationMessage').should('be.oneOf', 
+        ['Preencha este campo.', 'Marque esta caixa se deseja continuar.', 'Selecione um item da lista.']);
+})
+
+Cypress.Commands.add('verificarMensagemCampoEmail', (seletor, mensagens) => {
+    cy.get(seletor)
+        .focus()
+        .blur()
+        .then(($el) => {
+            const message = $el.prop('validationMessage');
+            const mensagemValida = mensagens.some((regex) => regex.test(message));
+      
+            expect(mensagemValida, `Mensagem capturada: "${message}"`).to.be.true;
+        });
+  });
